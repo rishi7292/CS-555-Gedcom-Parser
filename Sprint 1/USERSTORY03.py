c4ad454bd3ca4aae197f92af0a922b31906dccd1
Rishi Manjrekar <rishimanjrekar7@gmail.com>
2017-10-03 20:22:43 -0400

    User Story 03
    Birth should occur before death of an individual

---
+++ Sprint 1/USERSTORY03.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 02 14:41:56 2017
+
+@author: rishi
+"""
+import sqlite3
+conn=sqlite3.connect('GEDCOM_DATA.db')
+conn.text_factory = str
+def Story03():
+    query1="select NAME,AGE from INDIVIDUAL"
+
+    result1=conn.execute(query1)
+
+    value=result1.fetchall()
+    first=value[0]
+#def check(name):
+    for row in value:
+        if(row[1]<=0):
+            print ("ERROR : US03 :: "+row[0].replace("/"," ")+"has an invalid age since birthday is after death day")
+     #   else:
+      #      print ("ERROR: US03 :: "+row[0].replace("/"," ")+" has a valid age of ",row[1])
+Story03()
